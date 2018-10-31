47db6343d5967efe3c072653c1fa432252596a5f
cznccsjd <cznccsjd@126.com>
2018-10-31 15:39:20 +0800

    新增Readme文件：所有文件用途整理；新增mainFunc.java文件：学习main函数；

---
+++ helloworld/src/main/java/mainFunc.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class mainFunc {
+    public static void main(String[] args){
+        System.out.println("begin");
+        for (int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+
+    }
+}
